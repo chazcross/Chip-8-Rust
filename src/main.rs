28611e23e6422e13c01e7ae790366ad998d7fffa
cnewman <chrisnewman@quickenloans.com>
2019-09-24 13:05:01 -0400

    now displaying in a framebuffer

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::io;
 
 mod cpu;
-mod terminal;
+mod window;
 
 fn main() -> Result<(), io::Error> {
     let mut _cpu = cpu::CPU::new();
     _cpu.read_file();
 
-    let mut term = terminal::TerminalApp::new(_cpu);
-    term.run();
+    let mut gui = window::WindowApp::new(_cpu);
+    gui.run();
 
     Ok(())
 }
--- src/window/mod.rs
+++ src/window/mod.rs
@@ -1,11 +1,11 @@
 extern crate minifb;
-use minifb::{Key, Window, WindowOptions};
+use minifb::{Key, Scale, Window, WindowOptions};
 
 use super::cpu;
 use super::cpu::disassembler;
 
-const WIDTH: usize = 640;
-const HEIGHT: usize = 360;
+const WIDTH: usize = 64;
+const HEIGHT: usize = 32;
 
 pub struct WindowApp {
     cpu: cpu::CPU,
@@ -33,16 +33,34 @@ impl WindowApp {
             "Test - ESC to exit",
             WIDTH,
             HEIGHT,
-            WindowOptions::default(),
+            WindowOptions {
+                scale: Scale::X16,
+                ..WindowOptions::default()
+            },
         )
         .unwrap_or_else(|e| {
             panic!("{}", e);
         });
 
         while window.is_open() && !window.is_key_down(Key::Escape) {
-            for i in buffer.iter_mut() {
-                *i = 0; // write something more funny here!
+            let mut addr: u32 = 0;
+
+            self.cpu.do_cycle();
+
+            for y in 0..32 {
+                for x in 0..64 {
+                    if self.cpu.gfx[x][y] == true {
+                        buffer[addr as usize] = 0xFFF;
+                    } else {
+                        buffer[addr as usize] = 0x000;
+                    }
+                    addr += 1;
+                }
             }
+
+            // for i in buffer.iter_mut() {
+            //     *i = 0; // write something more funny here!
+            // }
 
             // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
             window.update_with_buffer(&buffer).unwrap();
