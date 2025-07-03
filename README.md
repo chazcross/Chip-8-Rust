5cb52b842e6d13fce19bc10a8fcb52666af7ff26
Chris Newman <chazcross@gmail.com>
2025-07-02 22:58:20 -0400

    updates

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 Chip 8 Emulator written in rust
 
-# Resources
+## Running the Program
+
+### Window Mode (Default)
+```bash
+cargo run
+```
+
+### Terminal Mode
+```bash
+cargo run -- --terminal
+```
+
+### Help
+```bash
+cargo run -- --help
+```
+
+## Resources
 http://mattmik.com/files/chip8/mastering/chip8.html
 https://en.wikipedia.org/wiki/CHIP-8
 
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 
+use clap::Parser;
+
 mod cpu;
 mod window;
 mod terminal;
 
+#[derive(Parser)]
+#[command(name = "chip8")]
+#[command(about = "A CHIP-8 emulator")]
+struct Args {
+    #[arg(long, help = "Run in terminal mode instead of window mode")]
+    terminal: bool,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    
     let mut _cpu = cpu::CPU::new();
     _cpu.read_file();
 
-    // let mut gui = window::WindowApp::new(_cpu);
-    // gui.run();
-
-    let mut term = terminal::TerminalApp::new(_cpu);
-    term.run()?;
+    if args.terminal {
+        let mut term = terminal::TerminalApp::new(_cpu);
+        term.run()?;
+    } else {
+        let mut gui = window::WindowApp::new(_cpu);
+        gui.run();
+    }
 
     Ok(())
 }
