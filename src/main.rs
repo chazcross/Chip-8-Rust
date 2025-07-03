128a67a2d2e8777d44908d5fc833d6d2d320ee1a
Chris Newman <chazcross@gmail.com>
2025-07-02 22:47:22 -0400

    add code to use old ui

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ use std::io;
 
 mod cpu;
 mod window;
+mod terminal;
 
 fn main() -> Result<(), io::Error> {
     let mut _cpu = cpu::CPU::new();
     _cpu.read_file();
 
-    let mut gui = window::WindowApp::new(_cpu);
-    gui.run();
+    // let mut gui = window::WindowApp::new(_cpu);
+    // gui.run();
+
+    let mut term = terminal::TerminalApp::new(_cpu);
+    term.run()?;
 
     Ok(())
 }
