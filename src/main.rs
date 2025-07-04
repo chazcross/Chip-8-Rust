3bc1c5228d91225aeec1fb484b3938b3b9a6993d
Chris Newman <chazcross@gmail.com>
2025-07-03 23:26:08 -0400

    terminal mode the default

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@ mod terminal;
 
 #[derive(Parser)]
 #[command(name = "chip8")]
-#[command(about = "A CHIP-8 emulator")]
+#[command(about = "A CHIP-8 emulator (terminal mode by default)")]
 struct Args {
-    #[arg(long, help = "Run in terminal mode instead of window mode")]
-    terminal: bool,
+    #[arg(long, help = "Run in window mode instead of terminal mode")]
+    window: bool,
 }
 
 fn read_rom_file(filename: &str) -> Result<Vec<u8>, std::io::Error> {
@@ -25,16 +25,16 @@ fn read_rom_file(filename: &str) -> Result<Vec<u8>, std::io::Error> {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     
-    if args.terminal {
-        let _cpu = cpu::CPU::new();
-        let mut term = terminal::TerminalApp::new(_cpu);
-        term.run()?;
-    } else {
+    if args.window {
         let mut _cpu = cpu::CPU::new();
         let rom_bytes = read_rom_file("roms/PONG.c8")?;
         _cpu.load_program(&rom_bytes);
         let mut gui = window::WindowApp::new(_cpu);
         gui.run();
+    } else {
+        let _cpu = cpu::CPU::new();
+        let mut term = terminal::TerminalApp::new(_cpu);
+        term.run()?;
     }
 
     Ok(())
